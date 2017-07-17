c29f147de06e0630eeca0e05060ad837ff3a78ad
Robert Murray and Victor Puentes <victor@>
2017-07-17 14:45:41 -0700

    add priority parameters to our to-do list.

---
+++ README.md
@@ -0,0 +1,5 @@
+| Behavior | Input | Output |
+| --- | --- | --- |
+| List to-dos | thing, other thing | <li>thing</li> <li>other thing</li> |
+| Add due date | finish by the end of the week | finish by 7/21/17  |
+| remove task once it's done | done button | done |
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
+//back-end logic
+
+//constructor that creates Task objects
 function Task(newTask, date) {
   this.theTask = newTask;
   this.dueDate = date;
 }
 
-function ClearInput() {
+function clearInput() {
   $("input#task").val("");
   $("input#completionDate").val("");
+  $("#priority").val("High")
 }
-
+//front-end logic
 $(function() {
 
   $("form#task-info").submit(function(event) {
@@ -15,7 +19,20 @@ $(function() {
 
     var inputtedTask = $("input#task").val();
     var inputtedDate = $("input#completionDate").val();
+    var inputtedPriority = $("#priority").val();
 
     var newTask = new Task(inputtedTask, inputtedDate);
+    debugger;
+
+    if (inputtedPriority === "High") {
+      
+      $("#to-do-high").append("<li><span class='thisTask'>" + newTask.theTask + " by " +  newTask.dueDate + "</span></li>");
+    } else if (inputtedPriority === "Medium") {
+      $("#to-do-medium").append("<li><span class='thisTask'>" + newTask.theTask + " by " +  newTask.dueDate + "</span></li>");
+    } else {
+      $("#to-do-low").append("<li><span class='thisTask'>" + newTask.theTask + " by " +  newTask.dueDate + "</span></li>");
+    }
+
+    clearInput();
   });
 });
